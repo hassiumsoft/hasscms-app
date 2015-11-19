1b80f6c80233ca694a6539b4d82796a05552493f
zhepama <zhepama@gmail.com>
2015-11-20 02:25:02 +0800

    提交本地配置文件

---
+++ app/config/backend/main-local.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'components' => [
+        'request' => [
+            'cookieValidationKey'=>'',
+        ],
+    ],
+];
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
+
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/app/modules/HassClassLoader.php');
+require(__DIR__ . '/app/config/bootstrap.php');
+require(__DIR__ . '/app/config/frontend/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/app/config/main.php'),
+    require(__DIR__ . '/app/config/main-local.php'),
+    require(__DIR__ . '/app/config/frontend/main.php'),
+    require(__DIR__ . '/app/config/frontend/main-local.php')
+);
+$application = new yii\web\Application($config);
+$application->run();
